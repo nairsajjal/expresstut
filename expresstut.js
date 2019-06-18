8c3905897c80c4f2dfd6b98b9f6f6fa1f6db20d5
sajjal <nairsajjal@gmail.com>
2019-06-18 16:08:59 +0530

    public folder added

---
+++ expresstut.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app =  express();
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req,res){
+        res.send('Working Express');
+});
+
+app.listen(app.get('port'), function() { 
+    console.log('Express started press ctrl-c  to terminate');
+});
